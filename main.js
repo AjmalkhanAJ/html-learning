9c1484a22c07de990c2ef5bbcfaea1075f6f86ab
AjmalKhan21 <ajmaldevofficial@gmail.com>
2025-01-24 19:46:00 +0530

    learning js

---
+++ main.js
@@ -0,0 +1,9 @@
+// let a = "5"
+// let b = '5'
+// console.log(a*=b)
+// console.log(5^3)
+console.log(7&4)
+console.log(5&&0)
+console.log(5&&6)
+console.log(5>7? "sabare":"ajmal")
+console.log(5||0)
