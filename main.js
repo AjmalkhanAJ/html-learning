081070ce38d5ee21846b22fb0b55616d47780c93
AjmalKhan21 <ajmaldevofficial@gmail.com>
2025-02-13 20:49:34 +0530

    js sub task

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-function show(a)
-{
-    c=0;
-    while(a)
-    {
-        c++;
-        a=Math.floor(a/10);
-    }
-    document.getElementById('ss').innerHTML=c;
 
-    }
+function aj(a){
+    var c=0;
+ var d=-1;
+ var b=1;
+ while(a){
+    c=d+b;
+    alert(c)
+    d=b;
+    b=c;
+    a--;
+ }
+}
+aj(10)
+
+
+
+// function sk(n){
+//     if(n==1){
+//         return 1;
+//     }
+//     else{
+//         return n*sk(n-1);
+//     }
+// }
+// console.log(sk(3))
+
+
+// let num=(a,b)=>{return a+b}
+// console.log(num(1,2));
