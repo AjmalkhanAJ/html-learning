03c4e66cd042e1fded689d6a91b87737053c819f
AjmalkhanAJ <ajmaldevofficial@gmail.com>
2025-02-26 18:50:55 +0530

    appendfile using filesystem

--- module/filesystem/fileread.js
+++ module/filesystem/fileread.js
@@ -1,8 +1,14 @@
-const fs=require('fs');
-let input = "welcome to vjsiddhu vlogs";
-fs.writeFile('dynamic.txt',fInput,(err)=>{
-    if (err)throw err;
-    else{
-        console.log("this file is updated with given data");
-    }
-})
+// const fs=require('fs');
+// let input = "welcome to vjsiddhu vlogs";
+// fs.writeFile('dynamic.txt',fInput,(err)=>{
+//     if (err)throw err;
+//     else{
+//         console.log("this file is updated with given data");
+//     }
+// })
+
+var fs = require ('fs')
+fs.appendFile('dynamic.txt', '\n ',(err)=>{
+    if (err) throw err;
+    console.log('vanakkam!');
+});
